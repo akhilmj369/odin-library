199a3593bd323f6c3f8d86bc7132dbb37859c9a5
akhilmj369 <akhilmj369@gmail.com>
2023-12-25 19:24:33 +0530

    intial phase completed

--- books.js
+++ books.js
@@ -1,1 +1,17 @@
-console.log("hello");
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let newBookbtn = document.querySelector("#new-book-btn");
+    newBookbtn.addEventListener("click", function () {
+        let newBookForm = document.querySelector("#new-book-form");
+        newBookForm.style.display = "block";
+    });
+});
